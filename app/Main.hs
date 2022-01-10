6b2b308944e0a8ab827b492929c4939970b0ec64
Mohammed Atallah <mo.atalla96@gmail.com>
2022-01-10 15:51:33 +0200

    refactor: split creating a socket/handling request into seperate units. made handling requests recusive

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,29 @@
 module Main where
-import Network
-
-
+import Network ( accept, PortNumber, Socket )
+import Network.Socket
 
 import System.IO
 
+main :: IO ()
 main = do
+    sock <- createSocket 8080
+    handleRequest sock
+
+createSocket :: PortNumber -> IO Socket 
+createSocket port = do
+    sock <- socket AF_INET Stream defaultProtocol
+    setSocketOption sock ReuseAddr 1  
+    bind sock (SockAddrInet port iNADDR_ANY)
+    listen sock 2
+    return sock
+
+
+handleRequest :: Socket -> IO ()
+handleRequest sock = do
     content <- readFile "app/index.html"
-    s <- listenOn (PortNumber 8080)
-    (h, hostName, portNum) <- Network.accept s
+    (h, hostName, portNum) <- Network.accept sock
     putStrLn $ "Connection from " ++ hostName ++ ":" ++ show portNum
     hPutStrLn h "HTTP/1.1 200 OK\r\n\r\n"
     hPutStr h content
     hClose h
+    handleRequest sock
