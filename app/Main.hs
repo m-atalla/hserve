d1a570823b6858df0477db01bd06379cb8631615
Mohammed Atallah <mo.atalla96@gmail.com>
2022-01-12 00:38:32 +0200

    change demo site to resource path add next steps as "todos"

--- app/Main.hs
+++ app/Main.hs
@@ -11,16 +11,21 @@ main = do
 
 createSocket :: PortNumber -> IO Socket 
 createSocket port = do
+    -- AF_INET - IPv4 constant
     sock <- socket AF_INET Stream defaultProtocol
+    getSocketName sock
     setSocketOption sock ReuseAddr 1  
     bind sock (SockAddrInet port iNADDR_ANY)
     listen sock 2
     return sock
 
 
+-- TODO: Parallelise this procedure
+-- TODO: Handling request parameters
+-- TODO: Exception handling start.. 404 errors for starters..
 handleRequest :: Socket -> IO ()
 handleRequest sock = do
-    content <- readFile "app/index.html"
+    content <- readFile "resource/index.html"
     (h, hostName, portNum) <- Network.accept sock
     putStrLn $ "Connection from " ++ hostName ++ ":" ++ show portNum
     hPutStrLn h "HTTP/1.1 200 OK\r\n\r\n"
