3798961eb31f5fd421bc85be186dc2f580c631a6
Mohammed Atallah <mo.atalla96@gmail.com>
2022-02-22 15:10:49 +0200

    add STM to record # of response

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main (main) where
 
-import Control.Concurrent (forkFinally)
+import Control.Concurrent (forkFinally, threadDelay)
 import qualified Control.Exception as E
-import Control.Monad (forever, void)
+import Control.Monad (forever, void, forM_)
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as C
 import Network.Socket
@@ -10,8 +10,10 @@ import Network.Socket.ByteString(recv, sendAll)
 import HTTP
 import System.Directory (doesFileExist)
 import System.IO (withFile, IOMode (ReadMode), hFileSize)
-import Data.Word (Word16)
+import GHC.Conc
 
+
+type ThreadResult = TVar Int
 
 host :: HostName
 host = "127.0.0.1"
@@ -23,11 +25,12 @@ serviceURL :: HostName -> ServiceName -> String
 serviceURL h p = "http://" ++ h ++ ":" ++ p
 
 main :: IO ()
-main =
-    putStrLn ("Starting server on on:" ++ serviceURL host port) >>
-    runTCPServer (Just host) port hserve
+main = do
+    putStrLn ("Starting server on on:" ++ serviceURL host port)
+    responseTVar <- newTVarIO 0
+    runTCPServer (Just host) port hserve responseTVar
   where
-    hserve s = do
+    hserve s result = do
         msg <- recv s 1024
         let rawReq = unpackRequest msg
         let req = parseRequestHead . head $ rawReq
@@ -46,12 +49,20 @@ main =
 
             -- constructing Response
             let res = Response (ver req) code (statusMsg code) (resOHeaders path len)
-
-
             sendAll s $ packResponse (show res) resource
         else
-            C.readFile "resource/404.html" >>= \c -> sendAll s $ packResponse "HTTP/1.1 404 NOT FOUND\r\n\r\n" c
+            sendNotFoundResponse s
+
+        -- atomically allows performing STM actions inside IO actions
+        atomically $ commitTransaction result
+        update <- atomically $ readTVar result
+        putStr $ "\r" ++ show update
         close s
+
+
+sendNotFoundResponse :: Socket -> IO ()
+sendNotFoundResponse socket = C.readFile "resource/404.html" >>=
+    \c -> sendAll socket $ packResponse "HTTP/1.1 404 NOT FOUND\r\n\r\n" c
 
 -- Since ByteString is an instance of Semigroup it
 -- is able to concat with another ByteString using (<>) function
@@ -63,9 +74,15 @@ unpackRequest :: C.ByteString -> [String]
 unpackRequest msg = lines $ C.unpack msg
 
 
+-- Gets a TVar int
+commitTransaction :: ThreadResult -> STM ()
+commitTransaction result = do
+    instances <- readTVar result
+    writeTVar result (succ instances)
+
 -- Runs "server" handlers in parallel
-runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> IO a) -> IO a
-runTCPServer mhost port server = withSocketsDo $ do
+runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> ThreadResult -> IO a) -> ThreadResult -> IO a
+runTCPServer mhost port server threadResult = withSocketsDo $ do
     addr <- resolve
     E.bracket (open addr) close loop
   where
@@ -87,4 +104,4 @@ runTCPServer mhost port server = withSocketsDo $ do
             -- but 'E.bracketOnError' above will be necessary if some
             -- non-atomic setups (e.g. spawning a subprocess to handle
             -- @conn@) before proper cleanup of @conn@ is your case
-            forkFinally (server conn) (const $ close conn) 
+            forkFinally (server conn threadResult) (const $ close conn)
