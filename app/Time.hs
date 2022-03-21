44997c6dc8dba3b73b72e9aafd2f66be801b8fc2
Mohammed Atallah <mo.atalla96@gmail.com>
2022-03-21 03:59:56 +0200

    add server time function

---
+++ app/Time.hs
@@ -0,0 +1,11 @@
+module Time where
+
+import Data.Time.Clock ( getCurrentTime )
+import Data.Time.Format ( defaultTimeLocale, formatTime )
+
+-- Get Current server string time
+-- format is based on sepc found here:
+-- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Date
+getServerTime :: IO String
+getServerTime = do
+    formatTime defaultTimeLocale "%a, %d %b %Y %H:%M:%S GMT" <$> getCurrentTime
