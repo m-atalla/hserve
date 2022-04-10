dc0390730fafe36a5171827283187c3fae133adc
Mohammed Atallah <mo.atalla96@gmail.com>
2022-04-10 21:29:15 +0200

    add root path to Config

--- app/Config.hs
+++ app/Config.hs
@@ -1,10 +1,14 @@
-module Config (host, port) where
+module Config (host, port, root) where
 
 type HostName = String
 type ServiceName = String
+type RootPath = String
 
 host :: HostName
 host = "127.0.0.1"
 
 port :: ServiceName
 port = "3000"
+
+root :: RootPath 
+root = "resource/"
