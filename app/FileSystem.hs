b97882f951722651522ee2a476018e0ea055bbac
Mohammed Atallah <mo.atalla96@gmail.com>
2022-04-21 20:53:59 +0200

    add `FileSystem` module moved file system specific functions to this module

---
+++ app/FileSystem.hs
@@ -0,0 +1,30 @@
+module FileSystem where
+
+import qualified Config (root)
+import System.IO (withFile, IOMode (ReadMode), hFileSize)
+import Control.Exception ( try, SomeException )
+import HTTPTypes (Path, Request(..))
+
+-- joins paths of a directory
+-- sort of a safe (++) for file paths that avoids duplicated '/'
+(+/+) :: Path -> Path -> Path
+(+/+) a b =
+    if last a == '/' && head b == '/' then
+        init a ++ "/" ++ tail b
+    else
+        a ++ b
+
+-- Get File length
+fileLength :: FilePath -> IO (Maybe Integer)
+fileLength filePath = do
+    size <- try $ withFile filePath ReadMode hFileSize :: IO (Either SomeException Integer)
+    case size of
+        Left _ -> return Nothing
+        Right n -> return $ Just n
+
+
+resolvePath :: Request -> String
+resolvePath req = case reqPath of [] -> error "Invalid Path"
+                                  "/" -> Config.root +/+ "index.html"
+                                  reqPath -> Config.root +/+ reqPath
+    where reqPath = path req
